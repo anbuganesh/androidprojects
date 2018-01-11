a239bb480ac2e61ea1ca97f60b99033e0515a496
anbuganesh <anganesh@gmail.com>
2018-01-11 23:24:30 +0530

    adding members and expense completed.

--- app/src/main/java/com/example/ganesh/expenseshare/AddExpense.kt
+++ app/src/main/java/com/example/ganesh/expenseshare/AddExpense.kt
@@ -1,12 +1,16 @@
 package com.example.ganesh.expenseshare
 
+import android.app.DatePickerDialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
+import android.widget.TextView
 import io.realm.Realm
 import kotlinx.android.synthetic.main.activity_add_expense.*
+import java.text.DecimalFormat
+import java.text.SimpleDateFormat
 import java.util.*
 
 class AddExpense : AppCompatActivity() {
@@ -30,24 +34,57 @@ class AddExpense : AppCompatActivity() {
         val arrayadapter = ArrayAdapter(this,android.R.layout.simple_spinner_item,results)
 
         spinner!!.setAdapter(arrayadapter)
+        //Date field
+
+        val textView = findViewById<TextView>(R.id.editDate)
+
+        textView.text = SimpleDateFormat("dd.MM.yyyy").format(System.currentTimeMillis())
+
+
+        var cal = Calendar.getInstance()
+
+        val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
+            cal.set(Calendar.YEAR, year)
+            cal.set(Calendar.MONTH, monthOfYear)
+            cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+
+            val myFormat = "dd.MM.yyyy" // mention the format you need
+            val sdf = SimpleDateFormat(myFormat, Locale.US)
+            textView.text = sdf.format(cal.time)
+            println("hello $cal.getTime()")
+
+        }
+
+        textView.setOnClickListener {
+            DatePickerDialog(this@AddExpense, dateSetListener,
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)).show()
+        }
 
 
         var buttonaddExpense = findViewById<Button>(R.id.buttonSaveExpense)
 
         buttonaddExpense.setOnClickListener{
 
-            var expense = Expense(_ID= UUID.randomUUID().toString(),name = "name",amount=0.0,dateOfExpense = null,expenseDetails = "test")
+            var personName: String = spinner.selectedItem.toString()
+
+          //  val numberFormat = DecimalFormat("#.00")
+            println("test for double ${editTextAmount.text.toString().toDouble()}" )
+
+           // println("amount value ${editTextAmount.toString().toDouble()}")
 
 
+            var expense = Expense(_ID= UUID.randomUUID().toString(),name = personName,amount=editTextAmount.text.toString().toDouble(),dateOfExpense = cal.time,expenseDetails = editDetails.toString())
             expensemodel.addExpense(realm,expense)
             println("expense added success")
 
 
             var results = expensemodel.getExpenses(realm)
 
-            println("size ${results.count()}")
+           println("size ${results.count()}")
 
-            println(results[0]!!.name);
+            println(personName);
 
 
 
